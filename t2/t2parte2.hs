aae25d3d3b3fbec5927506b4ed787800da9c5382
gafantinel <37092806+gafantinel@users.noreply.github.com>
2018-04-01 20:04:30 -0300

    aux

--- t2/t2parte2.hs
+++ t2/t2parte2.hs
@@ -15,4 +15,4 @@ bin2dec [] = undefined
 bin2dec bits = auxBin2Dec bits ((length bits)-1)
 
 auxBin2Dec :: [Int] -> Int -> Int
-auxBin2Dec x = 
+auxBin2Dec (x,xs) = exp x + auxBin2Dec (head xs) (tail xs)
