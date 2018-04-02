e591ce68c156c57d2d59852c29df4ae417b557c3
gafantinel <37092806+gafantinel@users.noreply.github.com>
2018-04-02 20:14:33 -0300

    parte 3 completa

--- t2/t2parte3.hs
+++ t2/t2parte3.hs
@@ -3,3 +3,8 @@ soma x = sum (zipWith (*) (cycle [1,3]) x)
 
 decimal :: Int -> Int
 decimal x = head (filter (\n -> n >= x) [0,10..])
+
+isEanOk :: String -> Bool
+isEanOk x = 
+    if ((decimal (soma (map digitToInt x)) - soma (map digitToInt x)) == last (map digitToInt x)) then True
+    else False
