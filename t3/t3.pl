0f5c4797969f3351781fee19d8a39e0ced56189e
gafantinel <37092806+gafantinel@users.noreply.github.com>
2018-04-16 04:27:02 -0300

    6 errada, 7 incompleta

--- t3/t3.pl
+++ t3/t3.pl
@@ -33,3 +33,23 @@ zipmult(L1,L2,L3) :-
     H3 is H1 * H2,
     L3 = [H3 | T3],
     zipmult(T1,T2,T3).
+
+% 6
+% arrumar xxxxxxxxxxxxxxxxxxxxx
+potencias(N,L):-
+    potN0(N,L),
+    inverte(L,R).
+
+inverte([],[]). 
+inverte(L,R):-
+    L = [H|T],  
+    append(T2,[H],R),
+	inverte(T,T2).
+
+% 7
+positivos([],[]).
+positivos(L1,L2):-
+    L1 = [H1|T1],
+    L2 = [H2|T2],
+    append([H2],[H1],L2),
+    positivos(T1,T2).
