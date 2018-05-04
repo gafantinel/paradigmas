ee2dfd0d184dff148a89add922dbd9504991a621
gafantinel <37092806+gafantinel@users.noreply.github.com>
2018-05-04 20:28:56 -0300

    Update extra3.pl

--- extras/extra3.pl
+++ extras/extra3.pl
@@ -1,6 +1,24 @@
 %OBI2010
 
 % Escola Colorida
+
+/* 
+Uma escola ocupa um predio com seis andares, numerados
+de 1 a 6 (1 e o andar mais baixo, 6 o mais alto).
+Os alunos e professores decidiram em conjunto que
+cada andar sera pintado com uma cor diferente, entre
+amarelo, branco, rosa, azul, laranja e verde. Ficou
+tambem decidido que as seguintes condicoes devem ser
+obedecidas:
+• Branco deve ser usado para pintar ou o andar 3
+ou o andar 4.
+• O andar pintado de laranja e mais baixo do que
+o andar pintado de rosa.
+• O andar pintado de azul e mais baixo do que o
+andar pintado de laranja, mas mais alto do que
+o andar pintado de amarelo.
+*/
+
 /* Questao 1. Qual das alternativas abaixo e uma lista
 correta de cores para os andares da escola, do andar 1
 ao andar 6?
@@ -10,6 +28,7 @@ ao andar 6?
 (D) azul, amarelo, laranja, branco, rosa, verde. - predio([azul, amarelo, laranja, branco, rosa, verde]).
 (E) verde, amarelo, rosa, branco, azul, laranja. - predio([verde, amarelo, rosa, branco, azul, laranja]).
 */
+
 regra1(X) :-
     nth1(3,X,branco),
     nth1(3,X,branco).
